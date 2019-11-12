8fc9b4c0842fc4aeff985789057d81376ed02240
叶振东 <XanderYe@outlook.com>
2019-11-12 16:40:49 +0800

    添加主题获取分页功能

--- src/main/java/com/xander/mdblog/controller/TopicController.java
+++ src/main/java/com/xander/mdblog/controller/TopicController.java
@@ -1,19 +1,18 @@
 package com.xander.mdblog.controller;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageInfo;
 import com.xander.mdblog.base.ResultBean;
 import com.xander.mdblog.constant.Constants;
 import com.xander.mdblog.entity.Topic;
 import com.xander.mdblog.enums.ErrorCodeEnum;
-import com.xander.mdblog.exception.BusinessException;
 import com.xander.mdblog.service.TopicService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import net.sf.jsqlparser.statement.select.Top;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 import java.util.List;
@@ -47,6 +46,23 @@ public class TopicController {
         topic.setId(id);
         topicService.deleteTopic(topic);
         return new ResultBean();
+    }
+
+    /**
+     * 获取所有主题
+     *
+     * @param
+     * @return com.xander.mdblog.base.ResultBean
+     * @author yezhendong
+     * @date 2019-07-10
+     */
+    @ApiOperation(value = "分页获取所有主题", notes = "需要登录")
+    @GetMapping("getTopicPageList")
+    public ResultBean getTopicPageList(Integer page, Integer rows) {
+        Topic topic = new Topic();
+        topic.setDelFlag(Constants.IS_NOT_DELETED);
+        PageInfo<Topic> existTopicPageList = topicService.queryPageListByWhere(topic, page, rows);
+        return new ResultBean<>(existTopicPageList);
     }
 
     /**
