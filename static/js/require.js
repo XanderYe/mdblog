bbd0b1775fa3b98fb1458a29eef5387d2062b644
叶振东 <XanderYe@outlook.com>
2019-12-05 11:49:33 +0800

    增加cdn失效时本地文件引入

---
+++ static/js/require.js
@@ -0,0 +1,11 @@
+if (typeof jQuery == 'undefined') {
+  document.write(unescape("%3Cscript src='static/js/jquery.min.js'%3E%3C/script%3E"));
+}
+
+if (typeof Vue == 'undefined') {
+  document.write(unescape("%3Cscript src='static/js/vue.min.js'%3E%3C/script%3E"));
+}
+
+if (typeof VueRouter == 'undefined') {
+  document.write(unescape("%3Cscript src='static/js/vue-router.min.js'%3E%3C/script%3E"));
+}
